9878bac2325df1b9cd536b6d04050933e899facf
Ari Mendelow <ari@mendelow.me>
2019-06-23 15:43:53 -0500

    Use the 'setup' function to create a 'base_title' instance variable for reducing some repitition

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,30 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ruby on Rails Sample Application"
+  end
+
   # Let's test the Home page by issuing a GET request to the Static Pages 'home' URL
-  # and then making sure we recieve a 'success' status code in response
+  # and then making sure we recieve a 'success' status code in response.
+  # Also, test the page titles.
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Sample Application"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Sample Application"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Sample Application"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
