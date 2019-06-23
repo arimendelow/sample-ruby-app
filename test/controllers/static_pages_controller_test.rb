8ac94e77fd7a790b7d4604802aafb29721e006f6
Ari Mendelow <ari@mendelow.me>
2019-06-23 15:04:13 -0500

    Create a test for the 'about' page, which does not yet exist

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # Let's test the Home page by issuing a GET request to the Static Pages 'home' URL
+  # and then making sure we recieve a 'success' status code in response
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -8,6 +10,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get help" do
     get static_pages_help_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
     assert_response :success
   end
 
