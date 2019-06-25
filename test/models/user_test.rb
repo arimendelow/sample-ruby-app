3f4720a3fe59282588d2aa3b1db11d901d15b235
Ari Mendelow <ari@mendelow.me>
2019-06-25 17:03:42 -0500

    Add more tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,14 +4,27 @@ class UserTest < ActiveSupport::TestCase
   
   def setup
     @user = User.new(name: "Example User", email: "test@email.com")
-    @user1 = User.new(name: "", email: "notemailstructure")
   end
 
   test "should be valid" do
     assert @user.valid?
   end
 
-  test "should be invalid" do
-    assert @user.invalid?
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+    @user.name = "Example User"
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+    @user.email = "test@email.com"
+  end
+
+  test "email should be a valid email structure" do
+    @user.email = "missing at sign, has spaces, missing the domain extention"
+    assert_not @user.valid?
+    @user.email = "test@email.com"
   end
 end
