e7deaef4c7d9bda78e5b2b6c21025fff4560055f
Ari Mendelow <ari@mendelow.me>
2019-06-28 14:15:42 -0500

    Add end

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -75,4 +75,5 @@ class UserTest < ActiveSupport::TestCase
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 7
     assert_not @user.valid?
+  end
 end
