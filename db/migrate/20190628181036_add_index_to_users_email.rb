5d330a00e47285b539c0ce4fd8ea0d8a7b5a9272
Ari Mendelow <ari@mendelow.me>
2019-06-28 13:15:15 -0500

    Add the rule for adding the index to the email column in the users table, as well as enforcing uniqueness

--- db/migrate/20190628181036_add_index_to_users_email.rb
+++ db/migrate/20190628181036_add_index_to_users_email.rb
@@ -1,4 +1,7 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration[5.2]
   def change
+    # The 'add_index' method is adding an index on the email column of the user's table.
+    # The index itself doesn't enforce uniqueness, but the option 'unique: true' does
+    add_index :users, :email, unique: true
   end
 end
