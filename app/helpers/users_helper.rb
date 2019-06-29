7297bde37648bfe160213a14e494afcb22f767b0
Ari Mendelow <ari@mendelow.me>
2019-06-29 14:27:05 -0500

    Add a size to the gravatar URL

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,11 @@
 module UsersHelper
 
   # Returns the Gravatar (see https://en.gravatar.com) for a given user
-  def gravatar_for(user)
+  def gravatar_for(user, size: 50)
     # Convert the user's email to lowercase, get rid of leading and trailing whitespace, and get the MD5 hash of it, as that's what Gravatar URL's are based on. See here: http://en.gravatar.com/site/implement/hash/
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase.strip)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    # size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     # Image tag for the Gravatar with a 'gravatar' CSS class and alt text equal to the user's name
     image_tag(gravatar_url, alt: user.name, class:"gravatar")
   end
