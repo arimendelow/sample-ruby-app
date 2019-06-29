a13e5fa062785e5bd7aa4039dcd31a32c2af9d4a
Ari Mendelow <ari@mendelow.me>
2019-06-29 13:35:59 -0500

    Add user 'show' action as well as the cooresponding template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+
+    # Adds a 'byebug' prompt to the server terminal window, which we can issue commands to to figure out the state of the application
+    # debugger # - Uncomment this for debugging
+  end
+
   def new
   end
 end
