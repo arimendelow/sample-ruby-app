207d09a8c24dc85f9b7d59f9f58b558ece276936
Ari Mendelow <ari@mendelow.me>
2019-06-29 16:05:19 -0500

    Start to create the action for creating a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,21 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle save
+    else
+      render 'new'
+    end
+  end
+
+  private
+    # Returns a version of the 'params' hash with ONLY the permitted attributes, and raises an error if the :user attribute is missing
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
