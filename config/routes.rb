8ca003f61706279f34e02587de3e45a862edf53d
Ari Mendelow <ari@mendelow.me>
2019-06-29 13:23:48 -0500

    Add a Users resource

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/about'    ,to: 'static_pages#about'
   get '/contact'  ,to: 'static_pages#contact'
   get '/signup'   ,to: 'users#new'
+  resources :users # Need this for CRUD operations on a given user - see the CRUD table for the provided actions
 end
