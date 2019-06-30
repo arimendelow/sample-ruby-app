940f72d4973b0944e3bd98c2ce60c49003c56495
Ari Mendelow <ari@mendelow.me>
2019-06-29 19:36:21 -0500

    Add integration test for successful user submission

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,12 +7,29 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_no_difference 'User.count' do
       post users_path, params: {
         user: {
-          name: "",
-          email: "user@invalid",
-          password: "short",
-          password_confirmation: "different"
+          name:                   "",
+          email:                  "user@invalid",
+          password:               "short",
+          password_confirmation:  "different"
         }
       }
     end
+  end
+
+  test "valid signup information" do
+    get signup_path
+    # Test that there is a difference of 1in User.count before and after
+    assert_difference 'User.count', 1 do
+      post users_path, params: {
+        user: {
+          name:                   "Test User",
+          email:                  "user@example.com",
+          password:               "password",
+          password_confirmation:  "password"
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
   end
 end
