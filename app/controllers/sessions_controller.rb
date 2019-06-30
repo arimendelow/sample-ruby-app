b96a86171a0bcd88010f9a329d0355e5fa5af65d
Ari Mendelow <ari@mendelow.me>
2019-06-29 23:32:35 -0500

    Start to add the basics of logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    # ':session' is a hash within the params hash with the keys 'email' and 'password'
+    user = User.find_by(email: params[:session][:email].downcase)
+    # If the user exists AND the password is correct...
+    if user && user.authenticate(params[:session][:password])
+      # Log in and redirect to the user's show page
+    else
+      # Create an error message
+      render 'new'
+  end
+
+  def destroy
+  end
 end
