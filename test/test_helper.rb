52b9dc4583436589b27edb0f1881e68859b69c2d
Ari Mendelow <ari@mendelow.me>
2019-07-01 14:19:13 -0500

    Add methods for testing - log in as a particular user

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,4 +12,24 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  # Log in as a specific user
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # Log in as a specific user - this is the version that will be called in integration tests
+  # Same name as the other version of this method so that we can use code from a controller test in an integration without making changes to the login method
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: {
+      session: {
+        email: user.email,
+        password: password,
+        remember_me: remember_me,
+      }
+    }
+  end
 end
