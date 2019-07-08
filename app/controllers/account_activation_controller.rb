a230984ca8d81a64e797cf8e386a4daad8ead1a1
Ari Mendelow <ari@mendelow.me>
2019-07-08 14:37:00 -0500

    Add edit action for activating accounts

--- app/controllers/account_activation_controller.rb
+++ app/controllers/account_activation_controller.rb
@@ -1,2 +1,20 @@
 class AccountActivationController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[:email])
+    # If the user exists, but is not activated, and the correct activation token is passed in...
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      # Activate the user
+      user.update_attribute(:activated, true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      # Log the user in
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      # Otherwise, whatever was passed in as the activation token must not be correct, so notify the user
+      flash[:danger] = "Invalid activation link :("
+      redirect_to root_url
+    end
+  end
 end
