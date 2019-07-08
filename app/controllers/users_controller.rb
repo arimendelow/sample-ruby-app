6fef661d50fa902408a18cafd18d7d31897a540b
Ari Mendelow <ari@mendelow.me>
2019-07-08 16:14:46 -0500

    Remove a comment that was explaining a concept not specific to its location

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-
-    # Adds a 'byebug' prompt to the server terminal window, which we can issue commands to to figure out the state of the application
-    # debugger # - Uncomment this for debugging
   end
 
   def new
