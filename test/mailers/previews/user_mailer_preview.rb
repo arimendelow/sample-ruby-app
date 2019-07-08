fe6c44edbeb0ee32bb065ba0f35aeb93efafe5f2
Ari Mendelow <ari@mendelow.me>
2019-07-08 09:51:55 -0500

    Add a working preview method for account activation

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,7 +3,12 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
   def account_activation
-    UserMailer.account_activation
+    # The account_activation method requires a valid user object as an argument,
+    # so we define a 'user' var equal to the first user in the dev db, and then pass it as an arg
+    user = User.first
+    # Necessary because the account activation templates need an account activation token, which is a virtual attribute, so the user from the db doesn't have one
+    user.activation_token = User.new_token
+    UserMailer.account_activation(user)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
