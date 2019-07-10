faf01f3c3fe68da715d5326ead7046380a667e65
Ari Mendelow <ari@mendelow.me>
2019-07-10 13:38:48 -0500

    Replace two 'update_attribute' calls with one 'update_columns'

--- app/models/user.rb
+++ app/models/user.rb
@@ -89,8 +89,7 @@ class User < ApplicationRecord
   # For resetting a user's password
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
   # Send the password reset email
