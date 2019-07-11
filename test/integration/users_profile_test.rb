d2a12e32b35bc41bae050a1ebda491ad106dd21d
Ari Mendelow <ari@mendelow.me>
2019-07-11 15:18:23 -0500

    Add test for a user's profile display, complete with microposts

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UsersProfileTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include ApplicationHelper # Gets us the 'full_title' helper
+
+  def setup
+    @user = users(:ari)
+  end
+
+  test "profile display" do
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title', full_title(@user.name)
+    assert_select 'h1', text: @user.name
+    # Check for an image tag with class 'gravatar' inside of an h1 tag
+    assert_select 'h1>img.gravatar'
+    assert_match @user.microposts.count.to_s, response.body
+    assert_select 'div.pagination'
+    @user.microposts.paginate(page: 1).each do |micropost|
+      assert_match micropost.content, response.body
+    end
+  end
 end
