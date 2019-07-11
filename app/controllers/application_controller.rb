cfd8b8d8a841e41df94fcf45d8d497c81ae19288
Ari Mendelow <ari@mendelow.me>
2019-07-11 15:40:12 -0500

    Add comment and protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   include SessionsHelper
 
   private
 
+    # Confirms a logged in user
     def logged_in_user
       unless logged_in?
         store_location # Store the website that the user is trying to access - this function is a sessions helper
