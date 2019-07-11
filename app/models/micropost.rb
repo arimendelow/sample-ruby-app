77e3e8a2fa7bd5f268fc4c8b34fa38af98465967
Ari Mendelow <ari@mendelow.me>
2019-07-11 09:44:10 -0500

    Add a content presence and length validation to the microposts model

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,6 @@ class Micropost < ApplicationRecord
   belongs_to :user
   # Make sure that each micropost is tied to a user
   validates :user_id, presence: true
+  # Make sure that content is both not empty and does not exceed 140 characters
+  validates :content, presence: true, length: { maximum: 140 }
 end
