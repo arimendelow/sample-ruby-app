05302c4b80d7ae32d4806d993022b8316652550a
Ari Mendelow <ari@mendelow.me>
2019-07-17 20:26:59 -0500

    AWS SES config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,19 +70,23 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Configuration for using SendGrid in production
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  host = 'https://ari-webtest.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true,
-  }
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # host = 'https://ari-webtest.herokuapp.com'
+  # config.action_mailer.default_url_options = { host: host }
+  # ActionMailer::Base.smtp_settings = {
+  #   address: 'smtp.sendgrid.net',
+  #   port: '587',
+  #   authentication: :plain,
+  #   user_name: ENV['SENDGRID_USERNAME'],
+  #   password: ENV['SENDGRID_PASSWORD'],
+  #   domain: 'heroku.com',
+  #   enable_starttls_auto: true,
+  # }
+
+  # Configuration for using SES in production
+  mailer = AmazonSes::Mailer.new(:access_key => "AKIAIHIURD7GO5THTYYA", :secret_key => "BLZMZSlPzcMD0RALWNY45/zpFpJ4cgdPs6Uw35KusknJ")
+  config.action_mailer.delivery_method = mailer
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
