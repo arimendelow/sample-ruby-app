3f2a452112e686f210be3019c74fad3aab5ca315
Ari Mendelow <ari@mendelow.me>
2019-07-18 17:16:30 -0500

    Change account_activation route

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,18 @@ Rails.application.routes.draw do
   get     'password_resets/new'
   get     'password_resets/edit'
   root    'static_pages#home'
-  get     '/help'      ,to: 'static_pages#help'
-  get     '/about'     ,to: 'static_pages#about'
-  get     '/contact'   ,to: 'static_pages#contact'
-  get     '/signup'    ,to: 'users#new'
-  post    '/signup'    ,to: 'users#create'
-  get     '/login'     ,to: 'sessions#new'
-  post    '/login'     ,to: 'sessions#create'
-  delete  '/logout'    ,to: 'sessions#destroy'
+  get     '/help'                           ,to: 'static_pages#help'
+  get     '/about'                          ,to: 'static_pages#about'
+  get     '/contact'                        ,to: 'static_pages#contact'
+  get     '/signup'                         ,to: 'users#new'
+  post    '/signup'                         ,to: 'users#create'
+  get     '/login'                          ,to: 'sessions#new'
+  post    '/login'                          ,to: 'sessions#create'
+  delete  '/logout'                         ,to: 'sessions#destroy'
+  get     '/account_activation/:id/edit'    ,to: 'account_activation#edit'
   # Need this for CRUD operations on a given user - see the CRUD table for the provided actions
   resources :users
-  resources :account_activation,  only: [:edit]
+  # resources :account_activation,  only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
 end
