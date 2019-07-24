9be26aca5318ef84be8eee36818991acf847a455
Ari Mendelow <ari@mendelow.me>
2019-07-24 15:50:46 -0500

    Add user following and follower actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,6 +63,20 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
     # Returns a version of the 'params' hash with ONLY the permitted attributes, and raises an error if the :user attribute is missing
     def user_params
