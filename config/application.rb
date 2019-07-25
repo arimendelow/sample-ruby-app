5b2b061ce3ec6e9adeee0805dca232b44f0eb1a0
Ari Mendelow <ari@mendelow.me>
2019-07-25 11:58:36 -0500

    Add the configuration needed for graceful degradation of form submission

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module SampleApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Configuration needed for graceful degradation of form submission -
+    # Include the authenticity token in remote forms.
+    # For working when JS is disabled.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
